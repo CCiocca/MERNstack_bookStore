6b31ea76b62bffdb5a633a391390c00604204e5f
CCiocca <chiara.cioccarelli1@gmail.com>
2024-05-03 00:18:15 +0200

    routes to get all books and a single book

--- backend/index.js
+++ backend/index.js
@@ -1,7 +1,7 @@
 import express, { response } from "express";
 import {PORT, mongoDBURL} from "./config.js";
 import mongoose from "mongoose";
-import {Book} from "bookModel";
+import {Book} from "./models/bookModel.js";
 
 const app = express();
 
@@ -40,8 +40,38 @@ app.post('/books', async (request, response) => {
         console.log(error.message);
         response.status(500).send({message: error.message});
     }
-
 })
+
+//route to get all books
+app.get('/books', async (request, response) => {
+    try{
+        const books = await Book.find({})
+
+        return response.status(200).json({
+            count : books.length,
+            data: books,
+        });
+
+    } catch (error){
+        console.log(error.message);
+        response.status(500).send({message: error.message})
+    }
+})
+
+//route to get one book from database, through its ID
+app.get('/books/:id', async (request, response) => {
+    try{
+        const {id} = request.params;
+        const book = await Book.findById(id)
+
+        return response.status(200).json(book);
+
+    } catch (error){
+        console.log(error.message);
+        response.status(500).send({message: error.message})
+    }
+})
+
 
 mongoose
 .connect(mongoDBURL)
