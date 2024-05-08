c7d341377b5c1c4ce40e71c56358e0bb2e358762
CCiocca <chiara.cioccarelli1@gmail.com>
2024-05-08 17:50:49 +0200

    add bookSingleCard and BookModal components

--- frontend/src/components/home/BookCard.jsx
+++ frontend/src/components/home/BookCard.jsx
@@ -4,14 +4,14 @@ import {BiUserCircle} from 'react-icons/bi';
 import {AiOutlineEdit} from 'react-icons/ai';
 import {BsInfoCircle} from 'react-icons/bs';
 import {MdOutlineDelete} from 'react-icons/md';
-import BookSinglCard from './BookSinglCard';
+import BookSingleCard from './BookSingleCard';
 
 const BookCard = ({books}) => {
   return (
     //responsive responsive
     <div className='grid sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4'> 
       {books.map((item) => (
-        <BookSinglCard key={item._id} book={item}/>
+        <BookSingleCard key={item._id} book={item}/>
       ))}
     </div>
   )
